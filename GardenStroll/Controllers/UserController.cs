2ed83936be33ec679167902368ce6fb9d87f1fc1
Mohammad <mohammadamen.ababneh@gmail.com>
2024-05-18 05:03:14 +0300

    Edit Controleller

--- GardenStroll/Controllers/UserController.cs
+++ GardenStroll/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using GardenStroll.Data;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GardenStroll.Data;
+using GardenStroll.DTOs;
 using GardenStroll.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -8,26 +11,36 @@ using System.Linq;
 
 namespace GardenStroll.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class UserController : BaseApiController
     {
         private readonly DataContext _dataContext;
+        private readonly IMapper _mapper;
 
-        public UserController(DataContext dataContext)
+        public UserController(DataContext dataContext, IMapper mapper)
         {
             _dataContext = dataContext;
+            _mapper = mapper;
         }
 
         [HttpGet("GetAllUsers")]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUsers()
         {
-            return await _dataContext.Users.Include(user => user.Photos).ToListAsync();
+            var users =  await _dataContext.Users.Include(user => user.Photos).ToListAsync();
+
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(usersToReturn);
         }
 
         [HttpGet("GetUser/{id}")]
         public async Task<ActionResult<AppUser>> getUser(Guid id)
         {
-            return await _dataContext.Users.FindAsync(id);
+            var user = await _dataContext.Users.Where(x => x.Id == id)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+
+            return Ok(user);
         }
     }
 }
