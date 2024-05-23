a5d35b0970be39717bcba7fbddb26e20b258b4d5
Mohammad <mohammadamen.ababneh@gmail.com>
2024-05-23 19:35:20 +0300

    empty

--- GardenStroll/Helpers/AutoMapperProfiles.cs
+++ GardenStroll/Helpers/AutoMapperProfiles.cs
@@ -5,7 +5,7 @@ using GardenStroll.Entities;
 namespace GardenStroll.Helpers
 {
     public class AutoMapperProfiles : Profile
-    {
+    {   
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
